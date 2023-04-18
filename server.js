c3136e8ca0cf78caaacc38baf97eb1ac4f98e4ce
Kunal-Deep011 <117732649+Kunal-Deep011@users.noreply.github.com>
2023-04-18 19:57:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,35 +1,26 @@
-// intialising all variables
+
 const express = require("express");
-// set's up our express --> express allow me to create a server
+
 const app = express();
-//HTTP--.info exchange
+
 const server = require("http").Server(app);
 
-// above code--> creating a medium to send and recieve data
-
-//index.html file from public folder
 app.use(express.static("public"));
 
 
-// socket io intialisation-what is socket
+
 const io = require("socket.io")(server);
-// socket.io and combining it with my server.
 
-//socket io is my watchman,ehich keeps a register and track
-//every event,if anything happen, this waechman alerts everyone
-
-
- // a message is sent, so every one receive that message
 
 io.on("connection", (socket) =>{
-   // console.log(socket);
+   
     console.log("User connected:", socket.id);
 
 
-    //established connection
+    
 
    socket.on("chat message", (data) =>{
-       //watchman to give everyone msg
+      
        io.emit("chat message", data);
        console.log("message is being sent to everyone",data.message);
     });
